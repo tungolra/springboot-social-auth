9717165e5e2115f54bacf10faf7648da776a2590
rtungol <rarrtungol@gmail.com>
2023-02-17 11:19:40 -0500

    refactor code to use Java 17 and Spring Boot 3

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Spring Boot Social Authorization
 
 ### About
-Starter code for integrating social logins using GitHub and Google OAuth2.0 as authentication providers. jQuery is used on the front end, Bootstrap for minimal styling. 
+Starter code for integrating social logins using GitHub and Google OAuth2.0 as authentication providers. jQuery is used on the front end, Bootstrap for minimal styling. Originally following a Spring tutorial, I've updated the project to use Java 17 from Java 1.8 and Spring Boot 3 from Spring Boot 2.2.2.  
 
 ### What I Learned 
 - Some methods can no longer be used in newer versions of Java so I had to rebuild my POM file using Java 1.8 and Spring Boot 2.2.2 to follow this tutorial. 
@@ -9,14 +9,17 @@ Starter code for integrating social logins using GitHub and Google OAuth2.0 as a
 - Creating a Client Application that uses the authorization code grant to obtain access tokens from Authorization Servers. 
 - Conditional rendering using jQuery for HTML files
 
-
+### Further Iterations 
+- Reorganized the folder structure to follow Spring MVC best practices. 
+- Configured the project to connect to MongoDB to save Users to database. 
+ 
 ### Technologies
-- Java (1.8)
-- Spring Boot (2.2.2)
+- Java (17)
+- Spring Boot (3.0.2)
 - Spring Security 
 - Maven 
 - OAuth2
-- JQuery
+- ~~JQuery~~ 
 - Bootstrap (3.2) 
 - Thymeleaf
 - HTML
--- src/main/java/com/example/SocialApplication.java
+++ src/main/java/com/example/SocialApplication.java
@@ -1,60 +1,12 @@
 package com.example;
 
-import java.util.Collections;
-import java.util.Map;
-
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.security.web.authentication.HttpStatusEntryPoint;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
-@RestController
-public class SocialApplication extends WebSecurityConfigurerAdapter {
-
-	@GetMapping("/user")
-	public Map<String, Object> user(@AuthenticationPrincipal OAuth2User principal) {
-		return Collections.singletonMap("name", principal.getAttribute("name"));
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-				.authorizeRequests((authorizeRequests) -> authorizeRequests
-						.antMatchers("/", "/error", "/webjars/**").permitAll()
-						.anyRequest().authenticated())
-				.exceptionHandling((exceptionHandling) -> exceptionHandling
-						.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-				.csrf(c -> c
-						.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
-				.logout(l -> l
-						.logoutSuccessUrl("/").permitAll())
-				.oauth2Login(o -> o
-						.failureHandler((request, response, exception) -> {
-							request.getSession().setAttribute("error.message", exception.getMessage());
-							exception.printStackTrace();
-						}));
-	}
-
-	@GetMapping("/error")
-	public String error(HttpServletRequest request) {
-		String message = (String) request.getSession().getAttribute("error.message");
-		request.getSession().removeAttribute("error.message");
-		return message;
-	}
+public class SocialApplication{
 
 	public static void main(String[] args) {
-//		Dotenv dotenv = Dotenv.configure().directory("src/main/resources/templates/.env").load();
 		SpringApplication.run(SocialApplication.class, args);
 	}
 
